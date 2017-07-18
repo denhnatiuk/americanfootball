c242a22a483af9515fd4cb8d5372f0a34424ff7d
DenysHnaatiuk <denys.hnatiuk@gmail.com>
2017-07-18 14:36:23 +0300

    init

---
+++ fonts/dearJoe_II PC/JB gfx FONTS readme.txt
@@ -0,0 +1,31 @@
+J O E B O B graphics 2005
+
+Fontlovers everywhere,
+
+the JOEBOB graphics fonts are all freeware for personal use, but 
+if you want to use them for commercial purposses, you have to 
+notice me and/ or send an example of the work you've used them for.
+
+Only exceptions are the 'dearJoe' fonts, which are shareware; you 
+can download them for free to check them out, but if you want to 
+use 'em, you'll have to pay for a users-liscence. You can also ask me 
+about the pro-versions of these fonts.
+
+Even though I make these font available as freeware for personal use,
+they may not be re-distributed on any media, they may not be sold,  
+they may not be renamed or converted and they may not be made available
+for download on any web page/ftp site without my permission.
+
+E-mail me and ask for my permission, which you'll probably get.
+
+I shall, in no event, be liable for any damages arising out of the use 
+of these fonts.
+
+
+And now go on and enjoy the font.
+
+
+J O E B O B graphics
+www.joebob.nl
+
+info@joebob.nl
---
+++ js/main.js
@@ -0,0 +1,70 @@
+(function(){
+  // setup your carousels as you normally would using JS
+  // or via data attributes according to the documentation
+  // http://getbootstrap.com/javascript/#carousel
+  $('#carouselABC').carousel({ interval: 5000 });
+}());
+
+(function(){
+  $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<3;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
+$(document).ready(function(){
+  var parent = $('#galleryContent');
+  var div = $('<div/>'),
+      img = $('img');
+
+  $('#galleryContent img').on('click',function(){
+        var src = $(this).attr('src');
+        var img = '<img src="' + src + '" class="img-responsive"/>';
+        $('#myModal').modal();
+        $('#myModal').on('shown.bs.modal', function(){
+            $('#myModal .modal-body').html(img);
+        });
+        $('#myModal').on('hidden.bs.modal', function(){
+            $('#myModal .modal-body').html('');
+        });
+   });
+});
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
